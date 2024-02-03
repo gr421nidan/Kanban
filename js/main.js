8ac0bf458cdbc8ee902941d3359916a67e1542e6
gr421_nidan <nikushkina.dasha@yandex.ru>
2024-02-03 15:02:03 +0700

    Добавление метода перемещения в следующие колонки и метода возврата в предыдущую с указанием причины

--- js/main.js
+++ js/main.js
@@ -10,8 +10,9 @@ new Vue({
             completedTask: [],
             description: '',
             deadline: '',
-
-
+            reason: '',
+            dateCompleted: Date.now(),
+            check: true,
         };
     },
     methods: {
@@ -22,6 +23,7 @@ new Vue({
                     title: this.taskName,
                     description: this.description,
                     deadline: this.deadline,
+                    reason: this.reason
                 };
                 if (this.taskName !== '') {
                     newTask.createdDate = new Date().toLocaleDateString();
@@ -42,11 +44,36 @@ new Vue({
         //
         // },
 
-        moveTask(task){
+        moveFromPlanTask(task){
             this.planTask.splice(this.planTask.indexOf(task), 1)
             this.workTask.push(task)
-        }
+        },
+        moveFromWorkTask(task){
+            this.workTask.splice(this.workTask.indexOf(task), 1)
+            this.testingTask.push(task)
+        },
+        moveFromTestingTask(task){
+            this.reason=''
+            this.testingTask.splice(this.testingTask.indexOf(task), 1)
+            this.completedTask.push(task)
+            if(this.deadline>=this.dateCompleted){
+                this.check=true;
+            }
+            else{
+                this.check=false;
+            }
+        },
+        moveFromTestingTaskBack(task){
+            this.testingTask.splice(this.testingTask.indexOf(task), 1)
+            this.workTask.push(task)
 
+        }
+    },
+    computed: {
+        checked(){
+            return this.check
+        }
     }
+
 
 });
