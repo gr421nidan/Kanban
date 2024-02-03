a0e3349e31704b17140af670e122aa163b82120d
gr421nidan <nikushkina.dasha@yandex.ru>
2024-02-03 23:25:07 +0700

    Добавление возможности редактирования(не сохраняет)

--- js/main.js
+++ js/main.js
@@ -11,19 +11,26 @@ new Vue({
             description: '',
             deadline: '',
             reason: '',
-            dateCompleted: Date.now(),
-            check: true,
+            check: '',
+            dateCompleted: new Date().toLocaleDateString(),
+            editedTaskName: '',
+            editedDescription: '',
+            editedDeadline: '',
+            editedTaskModal: false,
+            editedTask:{}
         };
     },
     methods: {
 
         addTask() {
             if (this.taskName !== '') {
-                const newTask = {
+                const newTask={
+                    id: Date.now(),
                     title: this.taskName,
                     description: this.description,
                     deadline: this.deadline,
-                    reason: this.reason
+                    reason: this.reason,
+                    check: this.check,
                 };
                 if (this.taskName !== '') {
                     newTask.createdDate = new Date().toLocaleDateString();
@@ -33,16 +40,27 @@ new Vue({
                 this.taskName = '';
                 this.description = '';
                 this.deadline = '';
-
             }
         },
         removeTask(task, type){
             this.planTask.splice(this.planTask.indexOf(task), 1)
 
         },
-        // updateTask(task){
-        //
-        // },
+        editedTasks(task) {
+            this.editedTaskModal = true;
+            this.editedTask = task;
+            this.editedTaskName = task.title;
+            this.editedDescription = task.description;
+            this.editedDeadline = task.deadline;
+
+        },
+        saveEditedTask(newTask) {
+            const index = newTask.indexOf(this.editedTask);
+            newTask[index].title = this.editedTaskName;
+            newTask[index].description = this.editedDescription;
+            newTask[index].deadline = this.editedDeadline;
+            this.editedTaskModal = false;
+        },
 
         moveFromPlanTask(task){
             this.planTask.splice(this.planTask.indexOf(task), 1)
@@ -56,11 +74,10 @@ new Vue({
             this.reason=''
             this.testingTask.splice(this.testingTask.indexOf(task), 1)
             this.completedTask.push(task)
-            if(this.deadline>=this.dateCompleted){
-                this.check=true;
-            }
-            else{
-                this.check=false;
+            if (task.deadline >= task.createdDate){
+                task.check = 'Выполнено в срок'
+            }else {
+                task.check = 'Просроченно'
             }
         },
         moveFromTestingTaskBack(task){
@@ -69,11 +86,7 @@ new Vue({
 
         }
     },
-    computed: {
-        checked(){
-            return this.check
-        }
-    }
+
 
 
 });
